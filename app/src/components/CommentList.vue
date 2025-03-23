811c39767934a26d97763dcaead53f31f23fe13d
VladimirRudenk0 <131373045+VladimirRudenk0@users.noreply.github.com>
2025-03-23 11:10:41 +0300

    feat: компонент списка комментариев

---
+++ app/src/components/CommentList.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-list>
+    <v-list-item v-for="comment in comments" :key="comment.id">
+      <v-list-item-content>
+        <v-list-item-title>{{ comment.text }}</v-list-item-title>
+        <v-list-item-subtitle>{{ comment.create_date }}</v-list-item-subtitle>
+      </v-list-item-content>
+      <v-list-item-action>
+        <v-btn @click="editComment(comment.id)">Редактировать</v-btn>
+        <v-btn @click="deleteComment(comment.id)">Удалить</v-btn>
+      </v-list-item-action>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+export default {
+  props: {
+    comments: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    editComment(id) {
+      this.$emit('edit', id);
+    },
+    deleteComment(id) {
+      this.$emit('delete', id)
+    },
+  },
+};
+</script>

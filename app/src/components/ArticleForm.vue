9b01dc1ea088d3bd3f955c499573bcf9f42983f9
VladimirRudenk0 <131373045+VladimirRudenk0@users.noreply.github.com>
2025-03-23 11:00:54 +0300

    feat: компонент формы добавления статьи

---
+++ app/src/components/ArticleForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-form @submit.prevent="submitForm">
+    <v-text-field v-model="article.name" label="Название статьи" required></v-text-field>
+    <v-textarea v-model="article.article_text" label="Текст статьи" required></v-textarea>
+    <v-btn type="submit">Сохранить</v-btn>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      article: {
+        name: '',
+        article_text: '',
+      },
+    };
+  },
+  methods: {
+    submitForm() {
+      this.$emit('submit', this.article);
+    },
+  },
+};
+</script>
